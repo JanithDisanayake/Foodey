a5ffcf944c7ab3860f5ad9fa9adc95770018b954
Janith3003 <janith.kavinda3003@gmail.com>
2023-05-03 09:07:36 +0530

    added tailwind css

---
+++ src/routes/+page.svelte
@@ -0,0 +1,5 @@
+<script>
+</script>
+
+<h1 class="p-10 text-3xl text-center">Welcome to SvelteKit</h1>
+<p class="ml-10">Visit <a class="underline text-blue-500" href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
