3b5ddae265fa97aac147654b1c1a52a3434b8f32
Janith3003 <janith.kavinda3003@gmail.com>
2023-05-08 14:08:02 +0530

    backend GET by id

--- Go-Backend/main.go
+++ Go-Backend/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 	"github.com/gin-gonic/gin"
 )
 
@@ -13,6 +14,15 @@ func main() {
 
 	r.GET("/", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{"message": "Hello World !"})
+	})
+
+	r.GET("/:id", func(context *gin.Context) {
+		id, _ := strconv.Atoi(context.Param("id"))
+		fmt.Println(id)
+		context.JSON(http.StatusOK, gin.H{
+			"message": "Hello World !",
+			"id" : id,
+		})
 	})
 
 	r.Run(":3000")
