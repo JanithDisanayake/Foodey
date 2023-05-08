e7d710e8af6e69bb3e1a1c34043049b98868c620
Janith3003 <janith.kavinda3003@gmail.com>
2023-05-08 16:18:06 +0530

    sqlite and gorm installed to the backend

--- Go-Backend/main.go
+++ Go-Backend/main.go
@@ -2,19 +2,31 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net/http"
 	"strconv"
+
 	"github.com/gin-gonic/gin"
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 type User struct {
+	gorm.Model
 	ID   uint `gorm:"primaryKey;unique"`
 	Name string
 	Age  uint
 }
 
 func main() {
-	
+	db, err := gorm.Open(sqlite.Open("user.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to connect database")
+	} else {
+		fmt.Println(" 🚀 Database is created \n")
+	}
+	db.AutoMigrate(&User{})
+
 	r := gin.Default()
 	fmt.Print(" 🚀 Server is Up and Running \n\n")
 
@@ -27,11 +39,11 @@ func main() {
 		fmt.Println(id)
 		context.JSON(http.StatusOK, gin.H{
 			"message": "Hello World !",
-			"id" : id,
+			"id":      id,
 		})
 	})
 
-	r.POST("/", func(context *gin.Context)  {
+	r.POST("/", func(context *gin.Context) {
 		var user User
 		if err := context.ShouldBind(&user); err != nil {
 			context.String(http.StatusBadRequest, "bad request %v", err)
