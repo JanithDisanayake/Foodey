7c0fec95c7a62a1088b7bb9d67d240b0862039a5
Janith3003 <janith.kavinda3003@gmail.com>
2023-05-08 16:03:13 +0530

    backend POST request & User model

--- Go-Backend/main.go
+++ Go-Backend/main.go
@@ -7,10 +7,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type User struct {
+	ID   uint `gorm:"primaryKey;unique"`
+	Name string
+	Age  uint
+}
+
 func main() {
 	
 	r := gin.Default()
-	fmt.Println(" 🚀 Server is Up and Running \n")
+	fmt.Print(" 🚀 Server is Up and Running \n\n")
 
 	r.GET("/", func(context *gin.Context) {
 		context.JSON(http.StatusOK, gin.H{"message": "Hello World !"})
@@ -23,6 +29,15 @@ func main() {
 			"message": "Hello World !",
 			"id" : id,
 		})
+	})
+
+	r.POST("/", func(context *gin.Context)  {
+		var user User
+		if err := context.ShouldBind(&user); err != nil {
+			context.String(http.StatusBadRequest, "bad request %v", err)
+		}
+		fmt.Println(user)
+		context.String(http.StatusOK, "Hello %s", user.Name)
 	})
 
 	r.Run(":3000")
