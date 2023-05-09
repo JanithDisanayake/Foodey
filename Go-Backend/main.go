8b2bbf18a218744897a2f3e56ac0d13b936dedbc
Janith3003 <janith.kavinda3003@gmail.com>
2023-05-09 13:08:53 +0530

    change the users route

--- Go-Backend/main.go
+++ Go-Backend/main.go
@@ -19,26 +19,26 @@ type User struct {
 }
 
 func main() {
-	db, err := gorm.Open(sqlite.Open("user.db"), &gorm.Config{})
-	if err != nil {
-		log.Fatal("failed to connect database")
-	} else {
-		fmt.Println(" 🚀 Database is created \n")
-	}
-	db.AutoMigrate(&User{})
-
 	r := gin.Default()
 	fmt.Print(" 🚀 Server is Up and Running \n\n")
 
-	r.GET("/", func(context *gin.Context) {
+	db, err := gorm.Open(sqlite.Open("foodey.db"), &gorm.Config{})
+	if err != nil {
+		log.Fatal("failed to connect database")
+	} else {
+		fmt.Printf(" 🎯 Database is created \n\n")
+	}
+	db.AutoMigrate(&User{})
 
-		users := getAllUsers(db)
+	r.GET("/users", func(context *gin.Context) {
+		var users []User
+		db.Find(&users)
 
 		context.Writer.Header().Set("Access-Control-Allow-Origin", "*")
 		context.JSON(http.StatusOK, users)
 	})
 
-	r.GET("/:id", func(context *gin.Context) {
+	r.GET("/users/:id", func(context *gin.Context) {
 		id, _ := strconv.Atoi(context.Param("id"))
 		var user User
 		db.First(&user, "id = ?", id) // find product with integer primary key
@@ -47,53 +47,34 @@ func main() {
 		context.JSON(http.StatusOK, user)
 	})
 
-	r.POST("/", func(context *gin.Context) {
+	r.POST("/users", func(context *gin.Context) {
 		var user User
 		if err := context.ShouldBind(&user); err != nil {
 			context.String(http.StatusBadRequest, "bad request %v", err)
 		}
 		db.Create(&User{Name: user.Name, Age: user.Age})
-		fmt.Println(user)
+
 		context.JSON(http.StatusOK, user)
 	})
 
-	r.PATCH("/", func(context *gin.Context) {
+	r.PATCH("/users", func(context *gin.Context) {
 		var user User
 		if err := context.ShouldBind(&user); err != nil {
 			context.String(http.StatusBadRequest, "bad request: %v", err)
 		}
-		user = updateUser(db, user)
+		db.Save(user)
+
 		context.JSON(http.StatusOK, user)
 	})
 
-	r.DELETE("/:id", func(context *gin.Context) {
+	r.DELETE("/users/:id", func(context *gin.Context) {
 		id, _ := strconv.Atoi(context.Param("id"))
-		user := getUserById(db, id)
+		var user User
+		db.Take(&user, "id = ?", id)
 		db.Delete(&user)
+
 		context.String(http.StatusOK, "user deleted")
 	})
 
 	r.Run(":3000")
-}
-
-func getAllUsers(db *gorm.DB) []User {
-	var users []User
-	db.Find(&users)
-	return users
-}
-
-func getUserById(db *gorm.DB, id int) User {
-	var users []User
-	db.Find(&users)
-	for i := range users {
-		if users[i].ID == uint(id) {
-			return users[i]
-		}
-	}
-	return User{}
-}
-
-func updateUser(db *gorm.DB, user User) User {
-	db.Save(user)
-	return user
 }
