0f486d4eea3756eca5263ea63865eab2c1df2f80
Janith Disanayake <janithdisanayake@users.noreply.github.com>
2024-04-17 20:35:31 +0530

    move the handlers to the controllers/graphql package
    Signed-off-by: Janith Disanayake <janithdisanayake@users.noreply.github.com>

---
+++ Go-Backend/controllers/graphql/handler.go
@@ -0,0 +1,24 @@
+package graphql
+
+import (
+	"go-backend/graph"
+
+	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/playground"
+	"github.com/gin-gonic/gin"
+)
+
+func GraphqlHandler() gin.HandlerFunc {
+	h := handler.NewDefaultServer(graph.NewExecutableSchema(graph.Config{Resolvers: &graph.Resolver{}}))
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
+
+// Defining the Playground handler
+func PlaygroundHandler() gin.HandlerFunc {
+	h := playground.Handler("GraphQL", "/query")
+	return func(c *gin.Context) {
+		h.ServeHTTP(c.Writer, c.Request)
+	}
+}
