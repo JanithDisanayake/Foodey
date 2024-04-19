21648593bb2b65b2fbf10f2526ef6b73c3b28e42
Janith Disanayake <janithdisanayake@users.noreply.github.com>
2024-04-19 23:57:34 +0530

    configure the frontend the get the user data from graphql backend fix #7
    Signed-off-by: Janith Disanayake <janithdisanayake@users.noreply.github.com>

--- Svelte-Frontend/vite.config.js
+++ Svelte-Frontend/vite.config.js
@@ -1,6 +1,20 @@
-import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { sveltekit } from "@sveltejs/kit/vite";
+import { defineConfig } from "vite";
 
 export default defineConfig({
-	plugins: [sveltekit()]
+  plugins: [sveltekit()],
+  optimizeDeps: {
+    exclude: ["@urql/svelte"],
+  },
+  server: {
+    proxy: {
+      "/query": {
+        target: "http://localhost:3000/",
+        changeOrigin: true,
+        pathRewrite: {
+          "^/query": "/query",
+        },
+      },
+    },
+  },
 });
